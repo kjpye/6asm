ffc9789993c66d5bd1e9da9d1148bcd883b97238
Kevin Pye <Kevin.Pye@gmail.com>
2014-02-21 15:08:45 +1100

    wip

--- armtest.asm
+++ armtest.asm
@@ -26,6 +26,16 @@ fred
  str r1,[r2,r4]!
  str r1,[r2],r4
  ldr r1,[r2,r3,lsl#2]
- ldreq br1,[r6,#5]
+ ldreqb r1,[r6,#5]
  str r1,place
 place
+ ldmfd sp!,{r0,r1,r2}
+ stmia r0,{r0-r15}
+ ldmfd sp!,{r15}
+ ldmfd sp!,{r15}^
+ stmfd r13,{r0-r14}^
+ stmfd r13,{r0-r2,r6-r14}^
+ stmfd r13,{r2}^
+ stmfd sp!,{r0-r3,r14}
+ bl place
+ ldmed sp!,{r0-r3,r15}
